04139efb8564eebe2126cc9d074ecdbf59ea8928
krish-nair <krish.nair101@gmail.com>
2019-07-18 15:11:44 +1200

    navbar created with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import Navbar from "../src/components/navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pride-Clothing Project</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Navbar} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 

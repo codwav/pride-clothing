e860a4123349cf58f231027cf56d2d49e5d919dd
krish-nair <krish.nair101@gmail.com>
2019-07-31 18:32:51 +1200

    Fixed scrollbar effect added

--- src/components/home.js
+++ src/components/home.js
@@ -49,7 +49,7 @@ class Home extends Component {
                 </div>
               </div>
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Grid item xs={12} sm={12} md={10} lg={6}>
               <div className="bg-image">
                 <img
                   className="home-img"
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 
 export class Navbar extends Component {
+  state = {
+    scrolled: false
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== true) {
+        this.setState({
+          scrolled: true
+        });
+      } else
+        this.setState({
+          scrolled: false
+        });
+    });
+  }
+
+  //   componentWillMount() {
+  //     window.removeEventListener("scroll");
+  //   }
+
   render() {
     return (
-      <Grid container className="nav-menu">
-        <nav className="navbar">
+      <Grid container>
+        <div className={this.state.scrolled ? "navbar scrolled" : "navbar"}>
           <Link to="/">
-            <img alt="Pride Clothing NZ home page" className="logo" src={require("../images/prideclothing-logo-6-2.jpg")} width="250" />
+            <img
+              alt="Pride Clothing NZ home page"
+              className="logo"
+              src={require("../images/prideclothing-logo-6-2.jpg")}
+              width="250"
+            />
           </Link>
           <ul className="nav-links">
             <Link className="nav-text" to="/">
@@ -31,7 +57,7 @@ export class Navbar extends Component {
           <Link className="nav-text primary-button" id="product-design-button">
             Product Design
           </Link>
-        </nav>
+        </div>
       </Grid>
     );
   }
